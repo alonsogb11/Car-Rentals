4ef4a09dfc18324121690ae0641a9788df86b4ad
alonsogb11 <alonsogb11604@gmail.com>
2025-03-10 11:44:47 -0400

    Cambios menores

--- RentACar/Controllers/EmpleadoController.cs
+++ RentACar/Controllers/EmpleadoController.cs
@@ -1,14 +1,99 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RentACar.Data;
+using RentACar.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RentACar.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class EmpleadoController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDBContext _appDbContext;
+        public EmpleadoController(AppDBContext appDbContext)
         {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<Empleado> lista = await _appDbContext.Empleados.ToListAsync();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Nuevo()
+        {
+            ViewBag.TandaLabor = Enum.GetValues(typeof(TandaLabor))
+                .Cast<TandaLabor>()
+                .Select(tl => new SelectListItem
+                {
+                    Text = tl.ToString(),
+                    Value = ((int)tl).ToString()
+                })
+                .ToList();
+
+            ViewBag.Estados = Enum.GetValues(typeof(Estado))
+                .Cast<Estado>()
+                .Select(e => new SelectListItem
+                {
+                    Text = e.ToString(),
+                    Value = ((int)e).ToString()
+                })
+                .ToList();
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empleado)
+        {
+            await _appDbContext.Empleados.AddAsync(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int Id)
+        {
+            ViewBag.TandaLabor = Enum.GetValues(typeof(TandaLabor))
+                .Cast<TandaLabor>()
+                .Select(tl => new SelectListItem
+                {
+                    Text = tl.ToString(),
+                    Value = ((int)tl).ToString()
+                })
+                .ToList();
+
+            ViewBag.Estados = Enum.GetValues(typeof(Estado))
+                .Cast<Estado>()
+                .Select(e => new SelectListItem
+                {
+                    Text = e.ToString(),
+                    Value = ((int)e).ToString()
+                })
+                .ToList();
+
+            Empleado empleado = await _appDbContext.Empleados.FirstAsync(e => e.Id == Id);
+            return View(empleado);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Empleado empleado)
+        {
+            _appDbContext.Empleados.Update(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar(int Id)
+        {
+            Empleado empleado = await _appDbContext.Empleados.FirstAsync(e => e.Id == Id);
+            _appDbContext.Empleados.Remove(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
         }
     }
 }
